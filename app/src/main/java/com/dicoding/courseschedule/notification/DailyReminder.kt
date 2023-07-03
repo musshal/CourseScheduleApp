94eb0010a67b626a18b93d56a96225a7b3765d76
Musthafa Faishal <musthafafaishal@gmail.com>
2023-07-03 21:54:43 +0700

    feat: implement todo 12 and 13

--- app/src/main/java/com/dicoding/courseschedule/notification/DailyReminder.kt
+++ app/src/main/java/com/dicoding/courseschedule/notification/DailyReminder.kt
@@ -1,13 +1,24 @@
 package com.dicoding.courseschedule.notification
 
+import android.app.AlarmManager
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.os.Build
 import androidx.core.app.NotificationCompat
 import com.dicoding.courseschedule.R
 import com.dicoding.courseschedule.data.Course
 import com.dicoding.courseschedule.data.DataRepository
+import com.dicoding.courseschedule.ui.home.HomeActivity
+import com.dicoding.courseschedule.util.ID_REPEATING
+import com.dicoding.courseschedule.util.NOTIFICATION_CHANNEL_ID
+import com.dicoding.courseschedule.util.NOTIFICATION_CHANNEL_NAME
+import com.dicoding.courseschedule.util.NOTIFICATION_ID
 import com.dicoding.courseschedule.util.executeThread
+import java.util.Calendar
 
 class DailyReminder : BroadcastReceiver() {
 
@@ -24,11 +35,28 @@ class DailyReminder : BroadcastReceiver() {
 
     //TODO 12 : Implement daily reminder for every 06.00 a.m using AlarmManager
     fun setDailyReminder(context: Context) {
-
+        val alarmManager = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
+        val intent = Intent(context, DailyReminder::class.java)
+        val pendingIntent = PendingIntent
+            .getBroadcast(context, 100, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+        val calendar = Calendar.getInstance()
+        calendar.set(Calendar.HOUR_OF_DAY, 6)
+        calendar.set(Calendar.MINUTE, 0)
+        calendar.set(Calendar.SECOND, 0)
+        alarmManager.setRepeating(
+            AlarmManager.RTC_WAKEUP,
+            calendar.timeInMillis,
+            AlarmManager.INTERVAL_DAY,
+            pendingIntent
+        )
     }
 
     fun cancelAlarm(context: Context) {
-
+        val alarmManager = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
+        val intent = Intent(context, DailyReminder::class.java)
+        val pendingIntent = PendingIntent
+            .getBroadcast(context, ID_REPEATING, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+        alarmManager.cancel(pendingIntent)
     }
 
     private fun showNotification(context: Context, content: List<Course>) {
@@ -39,5 +67,38 @@ class DailyReminder : BroadcastReceiver() {
             val courseData = String.format(timeString, it.startTime, it.endTime, it.courseName)
             notificationStyle.addLine(courseData)
         }
+
+        val intent = Intent(context, HomeActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        val pendingIntent = PendingIntent.getActivity(
+            context, 0,
+            intent,
+            PendingIntent.FLAG_ONE_SHOT or PendingIntent.FLAG_IMMUTABLE
+        )
+
+        val notificationManagerCompat = context
+            .getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        val builder = NotificationCompat.Builder(context, NOTIFICATION_CHANNEL_ID)
+            .setSmallIcon(R.drawable.ic_notifications)
+            .setContentTitle(context.getString(R.string.today_schedule))
+            .setContentText(context.getString(R.string.notification_message_format))
+            .setAutoCancel(true)
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .setContentIntent(pendingIntent)
+            .setStyle(notificationStyle)
+
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                NOTIFICATION_CHANNEL_ID,
+                NOTIFICATION_CHANNEL_NAME,
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
+            builder.setChannelId(NOTIFICATION_CHANNEL_ID)
+            notificationManagerCompat.createNotificationChannel(channel)
+        }
+
+        val notification = builder.build()
+        notificationManagerCompat.notify(NOTIFICATION_ID, notification)
     }
 }
