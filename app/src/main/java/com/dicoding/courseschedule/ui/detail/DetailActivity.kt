63881bc46867c7624ce08f7205de5cbb41042c5b
Musthafa Faishal <musthafafaishal@gmail.com>
2023-07-03 18:52:47 +0700

    fix: detail course no display

--- app/src/main/java/com/dicoding/courseschedule/ui/detail/DetailActivity.kt
+++ app/src/main/java/com/dicoding/courseschedule/ui/detail/DetailActivity.kt
@@ -3,8 +3,10 @@ package com.dicoding.courseschedule.ui.detail
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.TextView
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
 import com.dicoding.courseschedule.R
 import com.dicoding.courseschedule.data.Course
 import com.dicoding.courseschedule.util.DayName.Companion.getByNumber
@@ -23,8 +25,11 @@ class DetailActivity : AppCompatActivity() {
 
         val courseId = intent.getIntExtra(COURSE_ID, 0)
         val factory = DetailViewModelFactory.createFactory(this, courseId)
+        viewModel = ViewModelProvider(this, factory)[DetailViewModel::class.java]
 
-
+        viewModel.course.observe(this) {
+            showCourseDetail(it)
+        }
     }
 
     private fun showCourseDetail(course: Course?) {
@@ -33,6 +38,10 @@ class DetailActivity : AppCompatActivity() {
             val dayName = getByNumber(day)
             val timeFormat = String.format(timeString, dayName, startTime, endTime)
 
+            findViewById<TextView>(R.id.tv_course_name).text = courseName
+            findViewById<TextView>(R.id.tv_time).text = timeFormat
+            findViewById<TextView>(R.id.tv_lecturer).text = lecturer
+            findViewById<TextView>(R.id.tv_note).text = note
         }
     }
 
